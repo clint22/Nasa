8c4ea514daec6af4e6bb2687017bcdd2aaf7b525
Clint Paul <clintpaul89@gmail.com>
2023-03-19 17:31:38 +0530

    added dailog fragment to show extra description.

---
+++ app/src/main/java/com/clint/nasa/pictures/PicturesDescriptionFragment.kt
@@ -0,0 +1,37 @@
+package com.clint.nasa.pictures
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.clint.nasa.R
+import com.clint.nasa.core.Constants.INTENT_KEY_DESCRIPTION_DETAIL_EXTRA_NAME
+import com.clint.nasa.databinding.FragmentDescriptionDialogBinding
+import com.google.android.material.bottomsheet.BottomSheetDialogFragment
+
+class PicturesDescriptionFragment : BottomSheetDialogFragment() {
+    private lateinit var binding: FragmentDescriptionDialogBinding
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        binding = FragmentDescriptionDialogBinding.inflate(layoutInflater)
+        return binding.root
+    }
+
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        if (arguments != null) {
+            arguments?.getString(INTENT_KEY_DESCRIPTION_DETAIL_EXTRA_NAME)?.let {
+                binding.textViewDetailedDescription.text = it
+            }
+        }
+    }
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setStyle(STYLE_NORMAL, R.style.SheetDialog)
+    }
+}
