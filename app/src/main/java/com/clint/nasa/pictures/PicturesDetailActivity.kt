7e4f1997ff0b50212d520ce3500794903b1e9199
Clint Paul <clintpaul89@gmail.com>
2023-03-19 12:42:54 +0530

    fixed pr comments.

--- app/src/main/java/com/clint/nasa/pictures/PicturesDetailActivity.kt
+++ app/src/main/java/com/clint/nasa/pictures/PicturesDetailActivity.kt
@@ -3,12 +3,15 @@ package com.clint.nasa.pictures
 import android.content.Context
 import android.content.Intent
 import android.os.Bundle
-import android.util.Log
 import android.view.View
 import androidx.activity.viewModels
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
 import com.clint.nasa.R
+import com.clint.nasa.core.Constants.INTENT_KEY_PICTURE_EXTRA_NAME
+import com.clint.nasa.core.Constants.INTENT_KEY_PICTURE_EXTRA_POSITION_NAME
+import com.clint.nasa.core.Constants.ITEM_TRANSITION_MIN_SCALE
+import com.clint.nasa.core.Constants.ITEM_TRANSITION_TIME_IN_MILLIS
 import com.clint.nasa.core.discriteScrollView.DSVOrientation
 import com.clint.nasa.core.discriteScrollView.DiscreteScrollView
 import com.clint.nasa.core.discriteScrollView.InfiniteScrollAdapter
@@ -19,7 +22,6 @@ import com.clint.nasa.core.extensions.loadFromUrl
 import com.clint.nasa.core.extensions.observe
 import com.clint.nasa.core.extensions.parcelable
 import com.clint.nasa.databinding.ActivityPicturesDetailBinding
-import com.google.gson.Gson
 import com.skydoves.transformationlayout.TransformationCompat
 import com.skydoves.transformationlayout.TransformationLayout
 import dagger.hilt.android.AndroidEntryPoint
@@ -59,8 +61,6 @@ class PicturesDetailActivity : AppCompatActivity(),
     }
 
     private fun renderPictures(pictures: List<Pictures>?) {
-        Log.e("nasa_pictures", Gson().toJson(pictures))
-        Log.e("picture_pos", picturePosition.toString())
         this.pictures = pictures
         picturesAdapter.picturesList = pictures.orEmpty()
         binding.run {
@@ -68,12 +68,11 @@ class PicturesDetailActivity : AppCompatActivity(),
             picturesScrollView.addOnItemChangedListener(this@PicturesDetailActivity)
             infiniteAdapter = InfiniteScrollAdapter.wrap(picturesAdapter)
             picturesScrollView.adapter = infiniteAdapter
-            picturesScrollView.setItemTransitionTimeMillis(150)
+            picturesScrollView.setItemTransitionTimeMillis(ITEM_TRANSITION_TIME_IN_MILLIS)
             picturesScrollView.setItemTransformer(
-                ScaleTransformer.Builder().setMinScale(0.8f).build()
+                ScaleTransformer.Builder().setMinScale(ITEM_TRANSITION_MIN_SCALE).build()
             )
             picturesScrollView.scrollToPosition(picturePosition)
-            Log.e("picture_scroll_pos", infiniteAdapter.realCurrentPosition.toString())
             progressDiscreetScroll.visibility = View.GONE
             picturesScrollView.visibility = View.VISIBLE
         }
@@ -101,8 +100,8 @@ class PicturesDetailActivity : AppCompatActivity(),
     }
 
     companion object {
-        private const val pictureExtraName = "pictureExtraName"
-        private const val pictureExtraPositionName = "picturePositionExtraName"
+        private const val pictureExtraName = INTENT_KEY_PICTURE_EXTRA_NAME
+        private const val pictureExtraPositionName = INTENT_KEY_PICTURE_EXTRA_POSITION_NAME
         fun startActivity(
             context: Context,
             transformationLayout: TransformationLayout,
@@ -121,7 +120,6 @@ class PicturesDetailActivity : AppCompatActivity(),
         viewHolder: PicturesAdapterScroll.ViewHolder?,
         adapterPosition: Int
     ) {
-        Log.e("current_item_position", adapterPosition.toString())
         if (!loadedFirstTime) {
             val positionInDataSet = infiniteAdapter.getRealPosition(adapterPosition)
             val picture = pictures?.get(positionInDataSet)
